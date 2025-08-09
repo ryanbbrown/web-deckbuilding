9d7b7bbd554c77d61971a59b2672a77e3609db36
Ryan Brown <ryanburnettebrown@gmail.com>
2025-08-09 14:12:04 -0400

    feat: setup semantic-release with automatic PR title extraction

---
+++ release.config.cjs
@@ -0,0 +1,58 @@
+// release.config.cjs (or .js)
+module.exports = {
+    branches: ["main", "test-semantic-release"],
+    plugins: [
+      ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+      ["@semantic-release/release-notes-generator", {
+        preset: "conventionalcommits",
+        writerOpts: {
+          // 1) tag commits that look like a PR/merge commit (e.g., "... (#11)")
+          transform(commit) {
+            if (commit.header && /\(#\d+\)/.test(commit.header)) {
+              commit.isPrTitle = true;
+            }
+            return commit;
+          },
+          // 2) lift the first PR-title we find into the global context
+          finalizeContext(context, commits) {
+            const pr = commits.find(c => c.isPrTitle);
+            if (pr) context.prTitle = pr.header || pr.subject;
+            return context;
+          },
+          // 3) print PR title right under the version header, then continue as normal
+          headerPartial:
+            "## {{#if version}}{{version}}{{else}}{{currentTag}}{{/if}}{{#if date}} ({{date}}){{/if}}\n\n" +
+            "{{#if prTitle}}{{prTitle}}\n\n{{/if}}"
+          // (we keep the default mainTemplate & commit grouping so Features/Fixes render normally)
+        }
+      }],
+      [
+        "@semantic-release/changelog",
+        {
+          changelogFile: "CHANGELOG.md",
+          changelogTitle: "# Changelog\n\nAll notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines."
+        }
+      ],
+      [
+        "@semantic-release/npm",
+        {
+          npmPublish: false
+        }
+      ],
+      [
+        "@semantic-release/git",
+        {
+          assets: ["CHANGELOG.md", "package.json"],
+          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }
+      ],
+      [
+        "@semantic-release/github",
+        {
+          successComment: false,
+          failComment: false,
+          releasedLabels: false
+        }
+      ]
+    ]
+  };
