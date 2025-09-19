146b91b313b0e0574952e4aaa3eb056508a4311b
Ryan Brown <ryanburnettebrown@gmail.com>
2025-09-18 23:13:49 -0400

    feat: basic y-redis functionality in subtree

--- y-redis/demos/auth-express/rollup.config.js
+++ y-redis/demos/auth-express/rollup.config.js
@@ -1,20 +1,22 @@
-import nodeResolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
-export default [{
-  input: './demo.js',
-  output: {
-    name: 'test',
-    file: 'dist/demo.js',
-    format: 'esm',
-    sourcemap: true
+export default [
+  {
+    input: './demo.js',
+    output: {
+      name: 'test',
+      file: 'dist/demo.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      // @ts-ignore
+      nodeResolve({
+        mainFields: ['module', 'browser', 'main'],
+      }),
+      // @ts-ignore
+      commonjs(),
+    ],
   },
-  plugins: [
-    // @ts-ignore
-    nodeResolve({
-      mainFields: ['module', 'browser', 'main']
-    }),
-    // @ts-ignore
-    commonjs()
-  ]
-}]
+];
--- y-redis/src/index.js
+++ y-redis/src/index.js
@@ -1,3 +1,3 @@
-export * from './ws.js'
-export * from './api.js'
-export * from './server.js'
+export * from './ws.js';
+export * from './api.js';
+export * from './server.js';
